2baa230c767bf022aec16b626eb445f859d691dc
Pkules <omac69@hotmail.com>
2025-11-03 10:09:04 +0800

    Remove cpuminer binaries from repo and update .gitignore

--- tools/cpuminer-opt-nitro-25.3/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-make distclean || echo clean
-rm -f config.status
-./autogen.sh || echo done
-CFLAGS="-O3 -march=native -Wall" ./configure --with-curl
-make -j $(nproc)
-
-#strip -s cpuminer
